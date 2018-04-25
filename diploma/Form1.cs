476fd80ea995519ca198eebee811abf2b9788222
Maria Tisna <marytisna@gmail.com>
2018-04-25 15:54:00 +0300

    Added connection opening on form loading; added connection closing on form closing

--- diploma/Form1.cs
+++ diploma/Form1.cs
@@ -7,14 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace diploma
 {
     public partial class Form1 : Form
     {
+        private SqlConnection sqlConnection = null;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["diploma.Properties.Settings.KyivCityGuideDBConnectionString"].ConnectionString;
+            sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(sqlConnection != null && sqlConnection.State!=ConnectionState.Closed)
+            {
+                sqlConnection.Close();
+            }
         }
     }
 }
